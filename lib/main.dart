6fde95dedeeb7a3e6881e370bfae7775696184ad
Petar Iliev <petariliev2002@gmail.com>
2023-01-31 18:51:43 -0600

    Final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'quiz.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 void main() => runApp(Quizzler());
 
@@ -27,19 +31,63 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  Quiz quiz = Quiz();
+  List<Widget> scoreKeeper = [];
+
+  void updateQuestion(bool correct) {
+    setState(() {
+      if (correct) {
+        scoreKeeper.add(
+          Icon(
+            Icons.check,
+            color: Colors.green,
+          ),
+        );
+      } else {
+        scoreKeeper.add(
+          Icon(
+            Icons.close,
+            color: Colors.red,
+          ),
+        );
+      }
+
+      if (quiz.isFinished()) {
+        scoreKeeper = [];
+        Alert(
+          context: context,
+          type: AlertType.success,
+          title: "DONE",
+          desc: "You have completed the quiz.",
+          buttons: [
+            DialogButton(
+              child: Text(
+                "OKAY",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              onPressed: () => Navigator.pop(context),
+              width: 120,
+            )
+          ],
+        ).show();
+      }
+      quiz.nextQuestion();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-        const Expanded(
+        Expanded(
           flex: 5,
           child: Padding(
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quiz.getQuestion(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -54,7 +102,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: const EdgeInsets.all(15.0),
             child: TextButton(
               style: const ButtonStyle(
-                backgroundColor: MaterialStatePropertyAll(Colors.black),
+                backgroundColor: MaterialStatePropertyAll(Colors.green),
                 foregroundColor: MaterialStatePropertyAll(Colors.white),
               ),
               child: const Text(
@@ -65,7 +113,11 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                if (quiz.getAnswer() == true) {
+                  updateQuestion(true);
+                } else {
+                  updateQuestion(false);
+                }
               },
             ),
           ),
@@ -85,19 +137,19 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                if (quiz.getAnswer() == false) {
+                  updateQuestion(true);
+                } else {
+                  updateQuestion(false);
+                }
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  String text = "";
+  bool answer = false;
+
+  Question(String t, bool a) {
+    text = t;
+    answer = a;
+  }
+}
